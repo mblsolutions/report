e01a425bfaed67232650e246106577debb5e0a3f
Christian Innerbichler <christian.innerbichler@gmail.com>
2021-11-15 12:13:05 +0000

    fix command

--- src/Console/Commands/DispatchScheduledReportsCommand.php
+++ src/Console/Commands/DispatchScheduledReportsCommand.php
@@ -34,8 +34,7 @@ class DispatchScheduledReportsCommand extends Command
 
         $this->info('Running Report Schedules');
 
-        // TODO put back
-        $repository->all($this->date)->each(function (ScheduledReport $schedule) {
+        $repository->getScheduledReportsToRun($this->date)->each(function (ScheduledReport $schedule) {
             $this->info(
                 sprintf('Running Schedule for Report %s', $schedule->getKey())
             );
